f67f5dca5b2bcf33030d08a1186637ddbcaea4e4
gongyanwen <24300190044@m.fudan.edu.cn>
2025-11-27 17:22:18 +0800

    gyw基本完成，但运行test.py会超时

--- cpu.py
+++ cpu.py
@@ -2,8 +2,6 @@ import sys
 import re
 import json
 
-cpu = Simulator()
-cpu.PC
 
 class Simulator:
     def __init__(self):
@@ -21,9 +19,23 @@ class Simulator:
             8: 'r8', 9: 'r9', 10: 'r10', 11: 'r11',
             12: 'r12', 13: 'r13', 14: 'r14'
         }
+        self.head_map = {
+            0x0: self.execute_halt,
+            0x1: self.execute_nop,
+            0x2: self.execute_rrmovq_and_cmov,
+            0x3: self.execute_irmovq,
+            0x4: self.execute_rmmovq,
+            0x5: self.execute_mrmovq,
+            0x6: self.execute_calc,
+            0x7: self.execute_jump,
+            0x8: self.execute_callq,
+            0x9: self.execute_ret,
+            0xa: self.execute_pushq,
+            0xb: self.execute_popq
+        }
         self.stat = 1
         self.memory = {}
-        self.ZF = 0
+        self.ZF = 1  #从示例输出可知，在初始化时ZF默认为1
         self.SF = 0 
         self.OF = 0
 
@@ -34,10 +46,19 @@ class Simulator:
         instructions = []
 
         for line in content.split('\n'):
+            #去除注释
             code = line.split('|')[0].strip()
+
+            #针对文件中可能不出现代码只有注释的行，直接跳过
+            if code == "":
+                continue
 
             addr = code.split(':')[0].strip()
             machine_code = code.split(':')[1].strip()
+
+            #有些行是一个函数的地址开头，无机器码，也直接跳过
+            if machine_code == "":
+                continue
 
             addr = int(addr,16)
             machine_bytes = bytes.fromhex(machine_code)
@@ -52,45 +73,27 @@ class Simulator:
         return instructions
         
     def fetch(self):
+        #从PC读取头一个byte
+        try:
+            head = self.memory[self.PC]  # 直接访问，如果不存在会抛出KeyError
+            first = head >> 4
+            
+            self.head_map[first]()  
 
-        head = self.memory.get(self.PC)
+        except KeyError:
+            self.stat = 3  # 内存访问错误
 
-        first = head >> 4
-        second = head & 0xf
-
-        head_map = {
-            0x0: execute_halt,
-            0x1: execute_nop,
-            0x2: execute_rrmovq_and_cmov,
-            0x3: execute_irmovq,
-            0x4: execute_rmmovq,
-            0x5: execute_mrmovq,
-            0x6: execute_calc,
-            0x7: execute_jump,
-            0x8: execute_callq,
-            0x9: execute_ret,
-            0xa: execute_pushq,
-            0xb: execute_popq
-        }
-        
-        head_map[first]
-
-
-
-    def decode(self,key):
-
-        return self.registers.get(key)
 
     def read_byte(self,address):
 
-        return self.memory.get(address,0) #?
+        return self.memory.get(address,0) 
 
     def read_word(self, address, size=8):
         
-        """从内存读取一个字（小端序）8个字节"""
+        #小端法读取8个byte
         value = 0
         for i in range(size):
-            byte_val = self.read_byte(address + i,0)
+            byte_val = self.read_byte(address + i)
             value |= (byte_val << (i * 8))
         return value
 
@@ -106,7 +109,7 @@ class Simulator:
 
     def execute_halt(self):
         self.stat = 2
-        self.PC += 1
+        #halt不改变PC
 
     def execute_nop(self):
         self.PC += 1
@@ -174,11 +177,11 @@ class Simulator:
         
         offset = self.read_word(self.PC + 2, 8)
 
-        base_addr = self.registers[rA]
+        base_addr = self.registers[rB]
         addr = base_addr + offset
 
         value = self.read_word(addr,8)
-        self.registers[rB] = value
+        self.registers[rA] = value
 
         self.PC += 10
 
@@ -209,8 +212,8 @@ class Simulator:
             #更新条件码
             self.ZF = 1 if sub_value == 0 else 0
             self.SF = 1 if (sub_value & 0x8000000000000000) != 0 else 0
-            self.OF = 1 if (rA_value < 0 and rB_value > 0 and sum_value < 0) or \
-                           (rA_value > 0 and rB_value < 0 and sum_value > 0) else 0
+            self.OF = 1 if (rA_value < 0 and rB_value > 0 and sub_value < 0) or \
+                           (rA_value > 0 and rB_value < 0 and sub_value > 0) else 0
         elif second == 2:  #andq
 
             and_value = rA_value & rB_value
@@ -299,27 +302,95 @@ class Simulator:
 
 
     def execute_pushq(self):
+
         reg_byte = self.read_byte(self.PC + 1)
         rA = self.get_register(reg_byte >> 4)  
         value = self.registers[rA]
         rsp_value = self.registers["rsp"]
+
         new_rsp = rsp_value - 8
         self.registers["rsp"] = new_rsp #先移动，再赋值
         self.write_word(new_rsp, value) #默认值8，栈操作默认是8字节
+
         self.PC += 2
 
     def execute_popq(self):
         reg_byte = self.read_byte(self.PC + 1)
         rA = self.get_register(reg_byte >> 4)  # 高4位是寄存器编号
+
         rsp_value = self.registers["rsp"]
         value = self.read_word(rsp_value)
         new_rsp = rsp_value + 8
         self.registers[rA] = value
         self.registers["rsp"] = new_rsp #先赋值，再移动栈帧
+
         self.PC += 2
 
-    def run(self):
-        pass
+    def save_states(self):
+        #符号码
+        CC = {
+            "OF": self.OF,
+            "SF": self.SF,
+            "ZF": self.ZF
+        }
+        #内存
+        mem_state = {}
+        mem_bytes = sorted(self.memory.keys())
+        processed_blocks = set()
+
+        for mem_byte in mem_bytes:
+            mem_byte_head = mem_byte - (mem_byte % 8)  #找到每8个字节的头字节
+            if mem_byte_head not in processed_blocks:
+                value = 0
+                for i in range(8):
+                    new_value = self.memory.get(mem_byte_head + i,0) #小端法
+                    value |= (new_value << 8*i)
+                if value:  #只存储非0值
+                    mem_state[mem_byte_head] = value
+                    processed_blocks.add(mem_byte_head)
+        
+        #本次指令结束后所有部分的状态
+        all_state = {
+            "CC": CC,
+            "MEM": mem_state,
+            "PC": self.PC,
+            "REG": self.registers.copy(),
+            "STAT": self.stat
+        }
+        self.states.append(all_state)
+
+    def run(self,program_input):
+        #读文件
+        instructions = self.parse_yo_file(program_input)
+        
+        
+        # 设置初始PC为第一条指令的地址
+        self.PC = instructions[0]['address']
+        
+        # 执行循环
+        while self.stat == 1:  # 正常执行
+            # 解码并执行指令
+            self.fetch()
+            
+            # 保存执行后状态
+            self.save_states()
+            
+            # 检查是否应该停止
+            if self.stat != 1:
+                break
+        
+        return self.states
+
+
+
+def main():
+    #读取输入的文件
+    input_file = sys.stdin.read()
+
+    CPU = Simulator()
+
+    states_history = CPU.run(input_file)
+    json.dump(states_history,sys.stdout,indent=4)
 
 if __name__ == "__main__":
-    pass
+    main()
